7480290b5d8e54553509f7b06ea0afd7b2644a9b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-13 15:46:49 +0000

    make test code for Users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@sample.com")
+  end
+  
+  test "shoud be vaild" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
 end
