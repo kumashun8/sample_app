421a7dcd346a10fbadb3c1ec7f5198658c226403
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-13 16:28:21 +0000

    Add format for email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,4 +36,12 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
+  
+  test "email validation should reject invalid addresses" do
+    valid_addresses = %w[user@example,com USER_foo.COM A_US-ER_foo.bar.org first.last@foo. alice+bob@bazcn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
+    end
+  end
 end
