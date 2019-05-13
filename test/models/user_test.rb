683f27a95f67a83edc2683e17a37aa4f4372a1cc
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-13 16:11:26 +0000

    Fix test assertion

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
-    @user.email 0 valid_address
+      @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
