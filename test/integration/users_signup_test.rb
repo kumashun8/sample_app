bdbb2bdf8119b6728c9dee9a31d893c30f8bcc49
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-22 03:41:41 +0000

    add routing

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                           password_confirmation: "bar"}}
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.alert"
+    assert_select "ul"
   end
 end
