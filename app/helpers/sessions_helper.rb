5c6657ff85c05ccc08044bf958b4c18b9ab11d46
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-15 12:02:24 +0900

    puts session and remeber-me data

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,22 @@ module SessionsHelper
   # 記憶トークンcookieに対応するユーザを返す
   def current_user
     if (user_id = session[:user_id])
+      puts("---------------------------")
+      puts("session")
+      puts("")
+      puts(user_id)
+      puts("")
+      puts("")
+      puts("---------------------------")
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
+    if (user_id = cookies.signed[:user_id])
+      puts("***************************")
+      puts("remember me")
+      puts("")
+      puts(user_id)
+      puts("")
+      puts("")
+      puts("***************************")
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -37,5 +51,12 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+      puts("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+      puts("logout")
+      puts("")
+      puts(session[:user_id])
+      puts("")
+      puts("")
+      puts("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
   end
 end
