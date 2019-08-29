2b190109332617a3b5ceaf1c02b83b43c1fac34a
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-08-29 22:53:01 +0900

    create users index page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_path
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
