d3ab6c9fea78abe48c1680f372cf163a9bbe3bd7
kumashun8 <tiwo3wa4@gmail.com>
2020-03-16 12:21:17 +0900

    Revert "Update test but not clear"
    This reverts commit 809d34543e3dd9a97a1c53623960dcca4ce9a37c.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,13 @@
 
 # ユーザーのセッション情報に関するコントローラー
 class SessionsController < ApplicationController
-  def new; end
-
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      if user.activated?
-        # ユーザーログイン後にユーザ情報のページにリダイレクトする
-        log_in @user
-        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-        recdirect_back_or @user
-      else
-        message = 'Account not activated.'
-        message += 'Check your email for the activation link.'
-        flash[:warning] = message
-        redirect_to root_url
-      end
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザ情報のページにリダイレクトする
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      recdirect_back_or @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
