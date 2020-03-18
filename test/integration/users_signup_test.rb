343fcac52e87191221b965ddb36dad72690db3bf
kumashun8 <tiwo3wa4@gmail.com>
2020-03-18 15:46:55 +0900

    Create integration_test for password_reset

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,10 +10,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: '',
-                                          email: 'user@invalid',
-                                          password: 'foo',
-                                          password_confirmation: 'bar' } }
+      post signup_path, params: {
+        user: {
+          name: '',
+          email: 'user@invalid',
+          password: 'foo',
+          password_confirmation: 'bar'
+        }
+      }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
@@ -25,10 +29,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'valid signup information with account activation' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post signup_path, params: { user: { name: 'Example User',
-                                          email: 'user@sample.com',
-                                          password: 'password',
-                                          password_confirmation: 'password' } }
+      post signup_path, params: {
+        user: {
+          name: 'Example User',
+          email: 'user@sample.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
