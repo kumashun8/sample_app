b30d929fd56ae0275cdb2dcc10b075af86f311a5
kumashun8 <tiwo3wa4@gmail.com>
2020-03-18 21:49:34 +0900

    Update seed to adding microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,27 @@ User.create!(
   email: 'example@hoge.com',
   password: 'password',
   password_confirmation: 'password',
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
-99.times do |_n|
+99.times do |n|
   name = Faker::Name.name
-  email = 'example-#{n+1}@hoge.com'
+  email = "example-#{ n + 1 }@hoge.com"
   password = 'password'
   User.create(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
